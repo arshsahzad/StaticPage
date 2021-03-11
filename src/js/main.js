481368200015cf20e8f94f8963d1f8fa3701c80e
Arsh Sahzad <admin@arsh.dev>
2021-03-11 16:55:51 +0530

    Question Add

---
+++ src/js/main.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "HTML is Used to Create ____",
+    answer: "Web Page",
+    options: [
+      "Web Page",
+      "Web Server",
+      "High Level Program",
+      "Machine Language Program"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which is the First Internet Search Engine?",
+    answer: "Archie",
+    options: [
+      "WAIS",
+      "Archie",
+      "Google",
+      "Altavista",
+    ]
+  },
+    {
+    numb: 3,
+    question: "In Internet Terminology IP Means ____",
+    answer: "Internet Protocol",
+    options: [
+      "Internet Provider",
+      "Internet Protocol",
+      "Internet Procedure",
+      "Internet Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL Stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+];
